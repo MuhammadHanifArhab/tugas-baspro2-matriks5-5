66ded4d4a2a59037d589e05a02616ba812b8f7e4
MuhammadHanifArhab <arhabmuhammadhanif@gmail.com>
2025-04-16 19:41:37 +0700

    Update matriks 5×5.py

--- matriks 5×5.py
+++ matriks 5×5.py
@@ -1,13 +1,10 @@
-# Inisialisasi matriks kosong
 matriks = []
 
-# Membuat matriks 5x5 dengan menggunakan append
 for i in range(5):
     baris = []
     for j in range(5):
-        baris.append(i * 5 + j + 1)  # Mengisi angka 1 - 25. Bisa juga diganti dengan nilai lain
+        baris.append(i * 5 + j + 1)
     matriks.append(baris)
 
-# Print matriks
 for baris in matriks:
     print(baris)
